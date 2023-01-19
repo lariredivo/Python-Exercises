f0c4929ff985d90f1d789d89c601b24eed1dd282
lariredivo <57192319+lariredivo@users.noreply.github.com>
2023-01-19 18:28:41 -0300

    Create  main.py

---
+++ Exercise 13 - Print multiplication table form 1 to 10/main.py
@@ -0,0 +1,7 @@
+#Exercise from https://pynative.com/python-basic-exercise-for-beginners/
+#Exercise 13: Print multiplication table form 1 to 10
+
+for x in range(1,11):
+  for y in range(1,11):
+    print (x * y, end=" ")
+  print(" ")
