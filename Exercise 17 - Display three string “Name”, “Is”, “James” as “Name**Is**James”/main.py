faf5ecad7a600413d56cd6ef82cc1f778e99c164
lariredivo <57192319+lariredivo@users.noreply.github.com>
2023-01-19 18:56:58 -0300

    Create main.py

---
+++ Exercise 17 - Display three string “Name”, “Is”, “James” as “Name**Is**James”/main.py
@@ -0,0 +1,9 @@
+#Exercise from https://pynative.com/python-basic-exercise-for-beginners/
+#Exercise 17 - Display three string “Name”, “Is”, “James” as “Name**Is**James”
+#Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+
+string1 = "Name"
+string2 = "Is"
+string3 = "James"
+
+print(string1, string2, string3, sep="**")
