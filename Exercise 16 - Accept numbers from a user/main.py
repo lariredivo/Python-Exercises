ba312243ff19d67cfc7d171ac2f2ae4a60abaffd
lariredivo <57192319+lariredivo@users.noreply.github.com>
2023-01-19 18:53:29 -0300

    Create main.py

---
+++ Exercise 16 - Accept numbers from a user/main.py
@@ -0,0 +1,8 @@
+#Exercise from https://pynative.com/python-basic-exercise-for-beginners/
+#Exercise 16 - Accept numbers from a user
+#Write a program to accept two numbers from the user and calculate multiplication
+
+number1 = int(input("Write a number: "))
+number2 = int(input("Write another number: "))
+result = number1 * number2
+print("The result of the multiplication is:", result)
